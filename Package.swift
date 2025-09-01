67d8590810505145f931ad3e7d6e25f512b4a801
Timo <38291523+lovetodream@users.noreply.github.com>
2025-09-01 11:52:07 +0200

    explicitly add nio dependencies

--- Package.swift
+++ Package.swift
@@ -8,12 +8,16 @@ let package = Package(
         .library(name: "FCM", targets: ["FCM"]),
     ],
     dependencies: [
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.86.0"),
         .package(url: "https://github.com/vapor/jwt-kit.git", from: "5.1.2"),
         .package(url: "https://github.com/vapor/multipart-kit.git", from: "5.0.0-alpha"),
         .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.27.0"),
     ],
     targets: [
         .target(name: "FCM", dependencies: [
+            .product(name: "NIOCore", package: "swift-nio"),
+            .product(name: "NIOHTTP1", package: "swift-nio"),
+            .product(name: "_NIOFileSystem", package: "swift-nio"),
             .product(name: "JWTKit", package: "jwt-kit"),
             .product(name: "MultipartKit", package: "multipart-kit"),
             .product(name: "AsyncHTTPClient", package: "async-http-client"),
